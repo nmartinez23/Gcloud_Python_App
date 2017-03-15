c085e346b1190ba8be002e4b5042f16745dcca9f
Nick Martinez <23nicholasmartinez@gmail.com>
2017-03-15 14:33:30 -0700

    Define POST method

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
 import webapp2
 
 form="""
-<form action="/testform">
+<form method="post" action="/testform">
     <input name="q">
     <input type="submit">
 </form>
@@ -27,11 +27,13 @@ class MainPage(webapp2.RequestHandler):
         self.response.out.write(form)
 
 class TestHandler(webapp2.RequestHandler):
-    def get(self):
-        q = self.request.get("q")
-        self.response.out.write(q)
-        # self.response.headers['Content-Type'] = 'text/plain'
-        # self.response.out.write(self.request)
+    def post(self):
+        # q = self.request.get("q")
+        # self.response.out.write(q)
+
+        # shows HTTP request information in the browser
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(self.request)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),('/testform', TestHandler)], debug=True)
