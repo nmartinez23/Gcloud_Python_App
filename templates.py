5b5849f0dfef13b951ae76641107f2520179a02d
Nick Martinez <23nicholasmartinez@gmail.com>
2017-03-16 12:51:05 -0700

    Add shopping list to template, html escaping

--- templates.py
+++ templates.py
@@ -4,23 +4,8 @@ import jinja2
 import webapp2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
-
-
-hidden_html = """
-<input type="hidden" name="food" value="%s">
-"""
-
-item_html = "<li>%s</li>"
-
-shopping_list_html = """
-<br>
-<br>
-<h2>Shopping List</h2>
-<ul>
-%s
-</ul>
-"""
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -35,32 +20,7 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        self.render("shopping_list.html")
-
-    # code below used to test jinja syntax
-    # test code is in shopping_list.html
-    # def get(self):
-    #     n = self.request.get("n")
-    #     if n:
-    #         n = int(n)
-    #     self.render("shopping_list.html", n=n)
-
-
-        # output = form_html
-        # output_hidden = ""
-
-        # items = self.request.get_all("food")
-        # if items:
-        #     output_items = ""
-        #     for item in items:
-        #         output_hidden += hidden_html % item
-        #         output_items += item_html % item
-
-        #     output_shopping = shopping_list_html % output_items
-        #     output += output_shopping
-
-        # output = output % output_hidden
-
-        # self.write(output)
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items = items)
 
 app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
