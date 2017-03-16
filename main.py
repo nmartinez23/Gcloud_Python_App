cc81392776b5b2ec917c12d250b32a1537ee0ff6
Nick Martinez <23nicholasmartinez@gmail.com>
2017-03-15 22:34:57 -0700

    Add valid date methods and refactor post method

--- main.py
+++ main.py
@@ -14,13 +14,45 @@
 
 import webapp2
 
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+
+month_abbvs = dict((m[:3].lower(), m) for m in months)
+
+def valid_month(month):
+    if month:
+        short_month = month[:3].lower()
+        return month_abbvs.get(short_month)
+
+def valid_day(day):
+    if day and day.isdigit():
+        day = int(day)
+        if day > 0 and day <=31:
+            return day
+
+def valid_year(year):
+    if year and year.isdigit():
+        year = int(year)
+        if year > 1900 and year <= 2020:
+            return year
+
 form="""
 <form method="post">
     What is your birthday?
     <br>
-    <label> Month <input type="text" name="month"></label>
-    <label> Day <input type="text" name="day"></label>
-    <label> Year <input type="text" name="year"></label>
+    <label> Month <input type="text" name="month" value="%(month)s"></label>
+    <label> Day <input type="text" name="day" value="%(day)s"></label>
+    <label> Year <input type="text" name="year" value="%(year)s"></label>
     <div style="color: red">%(error)s</div>
     <br>
     <br>
@@ -29,55 +61,33 @@ form="""
 """
 
 class MainPage(webapp2.RequestHandler):
-    def write_form(self, error=""):
-        self.response.out.write(form % {"error": error})
+    def write_form(self, error="", month="", day="", year=""):
+        self.response.out.write(form % {"error": error,
+                                        "month": month,
+                                        "day": day,
+                                        "year": year})
 
     def get(self):
         self.write_form()
 
     def post(self):
-        user_month = valid_month(self.request.get('month'))
-        user_day = valid_day(self.request.get('day'))
-        user_year = valid_year(self.request.get('year'))
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
 
-        if not (user_month and user_day and user_year):
-            self.write_form("That doesn't look valid to me, friend.")
+        month = valid_month(user_month)
+        day = valid_day(user_day)
+        year = valid_year(user_year)
+
+        if not (month and day and year):
+            self.write_form("That doesn't look valid to me, friend.",
+                            user_month, user_day, user_year)
         else:
             self.response.out.write("That's a totally valid date!")
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
 
-# months = ['January',
-#           'February',
-#           'March',
-#           'April',
-#           'May',
-#           'June',
-#           'July',
-#           'August',
-#           'September',
-#           'October',
-#           'November',
-#           'December']
 
-# month_abbvs = dict((m[:3].lower(), m) for m in months)
-
-# def valid_month(month):
-#         if month:
-#             short_month = month[:3].lower()
-#             return month_abbvs.get(short_month)
-
-#     def valid_day(day):
-#     if day and day.isdigit():
-#         day = int(day)
-#         if day > 0 and day <=31:
-#             return day
-
-#     def valid_year(year):
-#     if year and year.isdigit():
-#         year = int(year)
-#         if year > 1900 and year <= 2020:
-#             return year
 
 
 # form="""
