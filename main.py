eab541c5155b17f38c98aa94511eeedab2507f54
Nick Martinez <23nicholasmartinez@gmail.com>
2017-03-16 09:23:37 -0700

    Add HTML escaping method and POST success redirect

--- main.py
+++ main.py
@@ -13,6 +13,21 @@
 # limitations under the License.
 
 import webapp2
+import cgi
+
+# Implementing HTML Escaping
+# def escape_html(s):
+#     for (i, o) in (("&", "&amp;"),
+#                    (">", "&gt;"),
+#                    ("<", "&lt;"),
+#                    ('"', "&quot;")):
+#         s = s.replace(i, o)
+#     return s
+
+# Refactored with cgi script
+
+def escape_html(s):
+    return cgi.escape(s, quote = True)
 
 months = ['January',
           'February',
@@ -46,6 +61,7 @@ def valid_year(year):
         if year > 1900 and year <= 2020:
             return year
 
+
 form="""
 <form method="post">
     What is your birthday?
@@ -63,9 +79,9 @@ form="""
 class MainPage(webapp2.RequestHandler):
     def write_form(self, error="", month="", day="", year=""):
         self.response.out.write(form % {"error": error,
-                                        "month": month,
-                                        "day": day,
-                                        "year": year})
+                                        "month": escape_html(month),
+                                        "day": escape_html(day),
+                                        "year": escape_html(year)})
 
     def get(self):
         self.write_form()
@@ -83,11 +99,13 @@ class MainPage(webapp2.RequestHandler):
             self.write_form("That doesn't look valid to me, friend.",
                             user_month, user_day, user_year)
         else:
-            self.response.out.write("That's a totally valid date!")
+            self.redirect("/thanks") # redirect to /thanks url
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+class ThanksHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("Thanks! That's a totally valid day!")
 
-
+app = webapp2.WSGIApplication([('/', MainPage), ('/thanks', ThanksHandler)], debug=True)
 
 
 # form="""
